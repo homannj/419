b00dd2184dd7e8deabfb94cace4ad12e735a1631
Kevin To <kevinto1989@gmail.com>
2016-01-24 21:58:56 +0000

    cleaned up an error in the mongo url

--- 419-server/config/database.js
+++ 419-server/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
 	// the database url to connect!
-	url: 'mongodb://cs419:osu2015Q1@ec2-54-153-28-110.us-west-1.compute.amazonaws.com/:27017/419'
+	url: 'mongodb://cs419:osu2015Q1@ec2-54-153-28-110.us-west-1.compute.amazonaws.com:27017/419'
 }
